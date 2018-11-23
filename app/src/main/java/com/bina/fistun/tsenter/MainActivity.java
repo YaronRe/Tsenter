6e2a3032d57b5ebfdea83af12c107ba09e2f182b
reshefya <reshefyaron@gmail.com>
2018-11-23 08:44:31 +0200

    first commit

---
+++ app/src/main/java/com/bina/fistun/tsenter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bina.fistun.tsenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
